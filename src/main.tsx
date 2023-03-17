c356be090ef26b2d7e05badc013f44ea4f3361fd
tot_dlinnyy <gnngr333@gmail.com>
2023-03-18 01:07:56 +0700

    feat: add ui libriry Chackra and add some architecture folders

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+import {ChakraProvider} from '@chakra-ui/react'
 import {createRoot} from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom'
 
-import router from './router/router'
+import theme from './app/theme'
+import router from './pages'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
+  <ChakraProvider theme={theme}>
+    <RouterProvider router={router} />
+  </ChakraProvider>
 )
