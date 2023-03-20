1678f1d7dd4f8a8cceebe04bdd0d0f344db30672
tot_dlinnyy <gnngr333@gmail.com>
2023-03-20 21:04:37 +0700

    feat: add storybook v7

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {ChakraProvider} from '@chakra-ui/react'
 import {createRoot} from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom'
 
-import theme from './app/theme'
+import theme from './app/theme/theme'
 import router from './pages'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
