e4934bb1e03b12b4800177d7e73bce475a74b14f
tot_dlinnyy <gnngr333@gmail.com>
2023-03-20 21:14:34 +0700

    feat: add react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import {ChakraProvider} from '@chakra-ui/react'
 import {createRoot} from 'react-dom/client'
+import {QueryClient, QueryClientProvider} from 'react-query'
 import {RouterProvider} from 'react-router-dom'
 
 import theme from './app/theme/theme'
 import router from './pages'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider theme={theme}>
-    <RouterProvider router={router} />
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </QueryClientProvider>
 )
