063bf53fe7d7fd63b4a390bc1bb6d80f8ba14323
tot_dlinnyy <gnngr333@gmail.com>
2023-03-24 03:21:02 +0700

    fix: fixx broken tests

--- src/entities/Tests/lib/helpers/getNextStage.test.ts
+++ src/entities/Tests/lib/helpers/getNextStage.test.ts
@@ -1,13 +1,14 @@
 import getNextStage from './getNextStage'
+import {EnterTestStages} from '../../consts/consts'
 
 describe('Test get next stage function', () => {
   test('Get stages in order', () => {
-    expect(getNextStage('info')).toBe('current')
-    expect(getNextStage('current')).toBe('wish')
-    expect(getNextStage('wish')).toBe('result')
+    expect(getNextStage('info', EnterTestStages)).toBe('current')
+    expect(getNextStage('current', EnterTestStages)).toBe('wish')
+    expect(getNextStage('wish', EnterTestStages)).toBe('result')
   })
 
   test('Get stage after last stage', () => {
-    expect(getNextStage('result')).toBe('result')
+    expect(getNextStage('result', EnterTestStages)).toBe('result')
   })
 })
--- src/entities/Tests/lib/helpers/getPrevStage.test.ts
+++ src/entities/Tests/lib/helpers/getPrevStage.test.ts
@@ -1,13 +1,14 @@
 import getPrevStage from './getPrevStage'
+import {EnterTestStages} from '../../consts/consts'
 
 describe('Test get prev stage function', () => {
   test('Get stages in order', () => {
-    expect(getPrevStage('result')).toBe('wish')
-    expect(getPrevStage('wish')).toBe('current')
-    expect(getPrevStage('current')).toBe('info')
+    expect(getPrevStage('result', EnterTestStages)).toBe('wish')
+    expect(getPrevStage('wish', EnterTestStages)).toBe('current')
+    expect(getPrevStage('current', EnterTestStages)).toBe('info')
   })
 
   test('Get stage before first stage', () => {
-    expect(getPrevStage('info')).toBe('info')
+    expect(getPrevStage('info', EnterTestStages)).toBe('info')
   })
 })
