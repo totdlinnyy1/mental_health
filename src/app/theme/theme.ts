edd825a0b97c4becadc370ac8b972b0ed214f84a
Nikita Panin <nik.vion@yandex.ru>
2023-03-23 21:35:44 +0700

    feat: added Stepper and Stapper.stories

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -1,10 +1,16 @@
 import {extendTheme} from '@chakra-ui/react'
+import {StepsTheme as Steps} from 'chakra-ui-steps'
 
 const config = {
   initialColorMode: 'light',
   useSystemColorMode: false
 }
 
-const theme = extendTheme({config})
+const theme = extendTheme({
+  config,
+  components: {
+    Steps
+  }
+})
 
 export default theme
--- src/entities/Tests/ui/Stepper/Stepper.tsx
+++ src/entities/Tests/ui/Stepper/Stepper.tsx
@@ -1,6 +1,28 @@
 import {ComponentType} from 'react'
-
+import {Step, Steps, useSteps} from 'chakra-ui-steps'
+import {Flex} from '@chakra-ui/react'
+const stepNumber = [
+  {id: 1},
+  {
+    id: 2
+  },
+  {
+    id: 2
+  }
+]
 const Stepper: ComponentType = () => {
-  return <div></div>
+  const {nextStep, prevStep, reset, activeStep} = useSteps({
+    initialStep: 1
+  })
+
+  return (
+    <Flex flexDir='column' width='100%'>
+      <Steps colorScheme='blue' activeStep={activeStep} background={'63B3ED'}>
+        {stepNumber.map(({id}) => (
+          <Step key={id}></Step>
+        ))}
+      </Steps>
+    </Flex>
+  )
 }
 export default Stepper
