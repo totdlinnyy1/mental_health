7dfc608abdc74c2f19074d5e874fae41661dd68a
tot_dlinnyy <gnngr333@gmail.com>
2023-03-23 22:32:28 +0700

    refactor: some changes to stepper

--- src/entities/Tests/ui/Stepper/Stepper.tsx
+++ src/entities/Tests/ui/Stepper/Stepper.tsx
@@ -1,28 +1,20 @@
+import {Step, Steps} from 'chakra-ui-steps'
 import {ComponentType} from 'react'
-import {Step, Steps, useSteps} from 'chakra-ui-steps'
-import {Flex} from '@chakra-ui/react'
-const stepNumber = [
-  {id: 1},
-  {
-    id: 2
-  },
-  {
-    id: 2
-  }
-]
-const Stepper: ComponentType = () => {
-  const {nextStep, prevStep, reset, activeStep} = useSteps({
-    initialStep: 1
-  })
+
+interface IProps {
+  activeStep: number
+  stepCount: number
+}
+
+const Stepper: ComponentType<IProps> = ({activeStep, stepCount}) => {
+  const steps = [...Array(stepCount).keys()]
 
   return (
-    <Flex flexDir='column' width='100%'>
-      <Steps colorScheme='blue' activeStep={activeStep} background={'63B3ED'}>
-        {stepNumber.map(({id}) => (
-          <Step key={id}></Step>
-        ))}
-      </Steps>
-    </Flex>
+    <Steps colorScheme='blue' activeStep={activeStep}>
+      {steps.map(step => (
+        <Step key={step} />
+      ))}
+    </Steps>
   )
 }
 export default Stepper
