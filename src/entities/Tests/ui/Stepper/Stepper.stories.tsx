66562aa1cf6a344d13e338796fe8c2d16a75a615
tot_dlinnyy <gnngr333@gmail.com>
2023-03-23 22:32:28 +0700

    refactor: some changes to stepper

--- src/entities/Tests/ui/Stepper/Stepper.stories.tsx
+++ src/entities/Tests/ui/Stepper/Stepper.stories.tsx
@@ -1,16 +1,22 @@
 import type {Meta, StoryObj} from '@storybook/react'
-import {withRouter} from 'storybook-addon-react-router-v6'
 import Stepper from './Stepper'
 
 const meta: Meta<typeof Stepper> = {
   title: 'entities/Tests/Stepper',
   component: Stepper,
   tags: ['autodocs'],
-  decorators: [withRouter]
+  args: {
+    activeStep: 1,
+    stepCount: 3
+  },
+  argTypes: {
+    activeStep: {control: 'number', description: 'Which step is active'},
+    stepCount: {control: 'number', description: 'How many steps'}
+  }
 }
 export default meta
 type Story = StoryObj<typeof Stepper>
 
 export const Primary: Story = {
-  render: () => <Stepper />
+  render: args => <Stepper {...args} />
 }
