b3d41c19e53a5ecd65fd239136e294a8ec519eeb
tot_dlinnyy <gnngr333@gmail.com>
2023-03-24 17:04:21 +0700

    fix: fix router and deploy to vercel

--- src/pages/Dashboard/DashBoaed.tsx
+++ src/pages/Dashboard/DashBoaed.tsx
@@ -1,9 +1,18 @@
 import {Container} from '@chakra-ui/react'
-import {ComponentType} from 'react'
+import {ComponentType, useEffect} from 'react'
 
 import {Layout} from '../../entities/Layout'
+import useEnterTestStore from '../../widgets/EnterTest/lib/hooks/useEnterTestStore'
+import {useNavigate} from 'react-router-dom'
 
 const Dashboard: ComponentType = () => {
+  const {isCompleted} = useEnterTestStore()
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!isCompleted) {
+      navigate('/info')
+    }
+  }, [isCompleted])
   return (
     <Layout>
       <Container />
