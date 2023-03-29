9deda593311c11d98b07a22161fcd09d863ba35d
Vladimir <haywaster3@gmail.com>
2023-03-29 21:52:43 +0700

    refactor: delete useless fuction, multiline structure from comment

--- src/entities/Tests/ui/ProgressBar/ProgressBar.test.tsx
+++
@@ -1,8 +0,0 @@
-import ProgressBar from './ProgressBar'
-import {render} from '@testing-library/react'
-
-describe('Test progress bar', () => {
-  test('render progress bar elem', () => {
-    render(<ProgressBar />)
-  })
-})
